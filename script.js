cb250757c7d512c761eaa51641f25160e2eaa47d
justinnitusj <justin0968376880@gmail.com>
2024-05-07 17:15:05 +0800

    Initial commit

---
+++ script.js
@@ -0,0 +1,12 @@
+let more_btn = document.querySelector('.btn-clk');
+let loginNext = document.querySelector('.login-next');
+more_btn.addEventListener('click', function() {
+    loginNext.style.display = 'block';
+    more_btn.style.display = 'none';
+});
+
+let continuous_btn = document.querySelector('.l-btn');
+continuous_btn.addEventListener('click',function () {
+    window.location.href = 'https://www.youtube.com/';
+});
+
